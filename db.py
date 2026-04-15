68fbf2a07283425f5653375da34b51c90c95a998
Saichand Linga <saichand@pranathibs.local>
2026-04-15 12:37:42 +0530

    added external database

--- db.py
+++ db.py
@@ -1,10 +1,29 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DATABASE_URL = "sqlite:///./meetings.db"
+
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# SessionLocal = sessionmaker(bind=engine)
+
+# Base = declarative_base()
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# 🔥 Read DB URL from environment (IMPORTANT)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = "sqlite:///./meetings.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(bind=engine)
+# Create engine
+engine = create_engine(DATABASE_URL)
 
+# Session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base
 Base = declarative_base()
